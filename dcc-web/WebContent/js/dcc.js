77c1da65aa48a40741b9e6cdae83c4dc07491929
Adam.Burton <Adam.Burton@hvvdproitw81d27.dorsetcc.local>
2017-04-13 14:28:05 +0100

    update

---
+++ dcc-web/WebContent/js/dcc.js
@@ -0,0 +1,17 @@
+$(function () {
+
+    $("body").addClass("js-enabled");
+
+    // show cookie message
+    if (!sessionStorage.seenCookieMessage || sessionStorage.seenCookieMessage == "no") {
+        $("#global-cookie-message").css("display", "block");
+    } else {
+        $("#global-cookie-message").css("display", "none");
+    }
+
+    // cookie message closed
+    $("#closeCookieButton").click(function () {
+        $("#global-cookie-message").css("display", "none");
+        sessionStorage.seenCookieMessage = "yes";
+    });
+})
---
+++ dcc-web/WebContent/js/forms.js
@@ -0,0 +1,42 @@
+   $(function () {
+       $(".error-summary").focus();
+       /* Radio button group, set classes to apply styles */
+       $(".selection-button-radio").change(function () {
+           var name = $(this).find("input").attr("name");
+           $("input[name=" + name + "]").closest(".block-label").removeClass("selected");
+           $(this).addClass("selected");
+       });
+
+       /* Checkbox, set classes to apply styles */
+       $("input[type=checkbox]").change(function () {
+           $(this).closest(".selection-button-checkbox").toggleClass('selected');
+
+       });
+
+
+       /* Set focus state classes to apply styles */
+       $("input[type=radio]").focus(function () {
+           $(this).closest('.selection-button-radio').addClass('focused');
+
+       });
+       $("input[type=radio]").focusout(function () {
+           $(this).closest('.selection-button-radio').removeClass('focused');
+       });
+
+       $("input[type=checkbox]").focus(function () {
+           $(this).closest('.selection-button-checkbox').addClass('focused');
+       });
+       $("input[type=checkbox]").focusout(function () {
+           $(this).closest('.selection-button-checkbox').removeClass('focused');
+       });
+
+
+       /* collapsable checkboxes, with accordion behaviour */
+       $(".radio-collapse").change(function () {
+           if ($(this).is(':checked')) {
+               $(this).closest(".radio").siblings('.radio').children('.collapse').removeClass('in');
+               $(this).closest(".selection-button-radio").next(".collapse").addClass("in");
+           }
+       });
+
+   });
