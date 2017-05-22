1460bb4e5cfd92583efdbfb0e85e7705b0aba7c7
Adam.Burton <Adam.Burton@hvvdproitw81d27.dorsetcc.local>
2017-05-22 16:14:59 +0100

    select table row work

--- dcc-web/WebContent/js/dcc.js
+++ dcc-web/WebContent/js/dcc.js
@@ -1,14 +1,12 @@
 $(function () {
-
     $("body").addClass("js-enabled");
-
     // show cookie message
     if (!sessionStorage.seenCookieMessage || sessionStorage.seenCookieMessage == "no") {
         $("#global-cookie-message").css("display", "block");
-    } else {
+    }
+    else {
         $("#global-cookie-message").css("display", "none");
     }
-
     // cookie message closed
     $("#closeCookieButton").click(function () {
         $("#global-cookie-message").css("display", "none");
--- dcc-web/WebContent/js/taglabels.js
+++ dcc-web/WebContent/js/taglabels.js
@@ -1,22 +1,16 @@
 $(document).ready(function () {
-
     /* ajax call to get items  */
     var tagItems = ["weymouth library", "bridport library", "dorchester library", "weymouth college", "preston community books", "chickerell library", "puddletown library", "portland library", "poole library", "parkstone library"];
-
     setTagItems();
-
     $("#tagSubmitBtn").click(function () {
-
         var selected = $("#tag").val().toLowerCase().trim();
-
-        if (selected == null || selected == "")
-            return;
-
+        if (selected == null || selected == "") return;
         var selectedInList = isSelectedInList(tagItems, selected);
         if (selectedInList >= 0) {
             tagItems.splice(selectedInList, 1);
             tagItems.unshift(selected);
-        } else {
+        }
+        else {
             tagItems.unshift(selected);
             tagItems.pop();
         }
@@ -28,17 +22,14 @@ $(document).ready(function () {
         $("#tag").val("");
         $("#submittedMessage").show();
         $("#submittedTag").html(selected);
-
     });
-
     $("#tagList").on("click", ".tag-label", function () {
         $("#tag").val($(this).text().toLowerCase());
     })
 
     function isSelectedInList(list, selected) {
         for (var i = 0; i < list.length; i++) {
-            if (selected == list[i])
-                return i;
+            if (selected == list[i]) return i;
         }
         return -1;
     }
@@ -46,9 +37,11 @@ $(document).ready(function () {
     function setTagItems() {
         var tagLabelsElement = $("#tagList");
         tagLabelsElement.html("");
-
         $.each(tagItems, function (key, value) {
-            tagLabelsElement.append("<span class=\"label label-primary tag-label\">" + value + "</span>");
+            tagLabelsElement.append("<span class=\"label label-primary tag-label\" tabindex=\"0\">" + value + "</span>");
         });
     }
+    $("#tagList").on("keypress", ".tag-label", function (e) {
+        if (e.which == 13) $("#tag").val($(this).text().toLowerCase());
+    })
 });
